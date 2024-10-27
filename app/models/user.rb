5da450f807ef316219dbcc253d62922af5b04fff
Pablo Guelfi <pguelfi@gmail.com>
2024-10-27 11:55:47 +0100

    Added first_name, last_name, and role to User.enum for user roles: donor (default)

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum role: { donor: 1, charity_admin: 2, evaluator: 3 }
+end
