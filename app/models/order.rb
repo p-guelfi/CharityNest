061a3f6a521b03f94ddc7006526b011b8a282bc3
Pablo Guelfi <pguelfi@gmail.com>
2024-11-03 17:02:13 +0100

    Revert "order model with routes and controller"
    This reverts commit 456222b3ffacb15ab1c5ca858460cb0ddd93730e.

--- app/models/order.rb
+++
@@ -1,5 +0,0 @@
-class Order < ApplicationRecord
-  belongs_to :user
-  belongs_to :charity_project
-  monetize :amount_cents
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,6 @@ class User < ApplicationRecord
 
   has_one :charity, dependent: :destroy
   has_many :donations
-  has_many :orders
 
   # Optionally, you can add a validation to ensure that only admin users can create charities
   def can_create_charity?
