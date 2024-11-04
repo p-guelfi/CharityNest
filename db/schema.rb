36cbfe29fd38d469e6b6793d56ead5f2657c0f4a
Pablo Guelfi <pguelfi@gmail.com>
2024-11-04 16:05:38 +0100

    m

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_03_164951) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_04_124545) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -81,6 +81,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_03_164951) do
     t.integer "amount_cents", default: 0, null: false
     t.string "state"
     t.string "checkout_session_id"
+    t.string "subscription_id"
     t.index ["charity_project_id"], name: "index_donations_on_charity_project_id"
     t.index ["user_id"], name: "index_donations_on_user_id"
   end
