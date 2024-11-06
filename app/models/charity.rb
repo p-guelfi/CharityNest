3a40c0d5e64200b280d7d0088a0bf0c5a3772b06
David Dicke <post@david-dicke.de>
2024-11-06 10:11:27 +0100

    photos dependent destroy

--- app/models/charity.rb
+++ app/models/charity.rb
@@ -3,7 +3,7 @@ class Charity < ApplicationRecord
   belongs_to :category
 
   has_many :charity_projects, dependent: :destroy
-  has_many_attached :photos
+  has_many_attached :photos, dependent: :destroy
 
   validates :name, presence: true
 end
