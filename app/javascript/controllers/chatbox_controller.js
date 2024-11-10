8e605a04e39e2b4e26ac43ae7ee624f7c84820cb
Pablo Guelfi <pguelfi@gmail.com>
2024-11-10 22:55:22 +0100

    changed send button for icon

--- app/javascript/controllers/chatbox_controller.js
+++ app/javascript/controllers/chatbox_controller.js
@@ -21,22 +21,8 @@ export default class extends Controller {
     sessionStorage.setItem("chatHistory", JSON.stringify(messages));
   }
 
-  // Toggle the modal visibility
-  toggleModal() {
-    const modal = document.getElementById("chatboxModal");
-    modal.classList.toggle("show"); // Show or hide the modal
-    document.body.classList.toggle("modal-open"); // Optional: Prevent scrolling when modal is open
-  }
-
-  // Close the modal (triggered by the close button or clicking outside)
-  closeModal() {
-    const modal = document.getElementById("chatboxModal");
-    modal.classList.remove("show");
-    document.body.classList.remove("modal-open"); // Optional: Allow scrolling when modal is closed
-  }
-
   sendMessage(event) {
-    const message = this.inputTarget.value;
+    const message = this.inputTarget.value.trim();
     if (message) {
       const messagesDiv = this.messagesTarget;
       let chatHistory = JSON.parse(sessionStorage.getItem("chatHistory")) || [];
