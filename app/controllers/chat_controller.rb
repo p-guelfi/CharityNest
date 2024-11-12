7fdbca44ca75931a387476811cca970311baf11f
Pablo Guelfi <pguelfi@gmail.com>
2024-11-12 11:30:08 +0100

    added fine-tuned model

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -8,8 +8,11 @@ class ChatController < ApplicationController
     client = OpenAI::Client.new(api_key: ENV['OPENAI_ACCESS_TOKEN'])
     response = client.chat(
       parameters: {
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: prompt }],
+        model: "ft:gpt-3.5-turbo-0125:personal:charitynest:ASiAsJL4",  # Fine-tuned model ID
+        messages: [
+          { role: "system", content: "You are CharityNest AI Assistant. CharityNest is a platform that facilitates donations to various charities and provides updates, news, and reports on the progress of specific projects. Users can also engage in forums related to each cause. As the AI assistant, your role is to help users with their donations, provide project updates, answer questions, and guide them through the platform's features." },
+          { role: "user", content: prompt }
+        ],
         max_tokens: 150
       }
     )
