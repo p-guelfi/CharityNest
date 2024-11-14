7fb053fae69e373269ab0fecc372748f13376e5e
David Dicke <post@david-dicke.de>
2024-11-14 17:36:17 +0100

    added description_long design of the grid

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_13_094528) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_14_150007) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -81,6 +81,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_13_094528) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "goal"
+    t.text "description_long"
     t.index ["charity_id"], name: "index_charity_projects_on_charity_id"
   end
 
