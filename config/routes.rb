918674b689518f8c3f1cdda5266dcbc70048c99e
michael laguerder <michaellukedelimlaguerder@yahoo.es>
2024-11-14 19:39:54 +0000

    about us button

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+
   # Defines the root path route ("/")
   # root "posts#index"
   resources :categories, only: [:new, :create] do
@@ -32,6 +33,10 @@ Rails.application.routes.draw do
     end
 
   end
+
+# config/routes.rb
+
+  get 'about', to: 'pages#about'
 
   mount StripeEvent::Engine, at: '/webhooks/stripe'
 
